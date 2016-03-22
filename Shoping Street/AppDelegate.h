593afa3939a7f27b31a119dff08f5c56616bdd55
WANGXINYU <wangxinyu@123.local>
2016-03-22 14:15:31 +0800

    Initial Commit

---
+++ Shoping Street/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Shoping Street
+//
+//  Created by WANGXINYU on 16/3/22.
+//  Copyright © 2016年 wangxinyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
